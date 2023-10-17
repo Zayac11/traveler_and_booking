83688792624888ee7465f6ef5c6b25d8b279c840
Zayac11 <brunogrbstudios@gmail.com>
2023-10-17 20:10:58 +0300

    added sf pro display font

--- frontend/src/widgets/Header/ui/Header.tsx
+++ frontend/src/widgets/Header/ui/Header.tsx
@@ -5,27 +5,24 @@ import s from './Header.module.scss'
 import { Navbar } from './Navbar/Navbar'
 import { ProfileHeader } from './ProfileHeader/ProfileHeader'
 
-export const Header = () => {
-    console.log('123')
-    return (
-        <Row>
-            <Col className={s.header} offset={4} span={16}>
-                <Row>
-                    <Col span={6}>
-                        <NavLink to='/'>
-                            <Space className={s.logo}>
-                                <Logo /> Traveler and Booking
-                            </Space>
-                        </NavLink>
-                    </Col>
-                    <Col span={12}>
-                        <Navbar />
-                    </Col>
-                    <Col span={6} style={{ textAlign: 'end' }}>
-                        <ProfileHeader />
-                    </Col>
-                </Row>
-            </Col>
-        </Row>
-    )
-}
+export const Header = () => (
+    <Row>
+        <Col className={s.header} offset={4} span={16}>
+            <Row>
+                <Col span={6}>
+                    <NavLink to='/'>
+                        <Space className={s.logo}>
+                            <Logo /> Traveler and Booking
+                        </Space>
+                    </NavLink>
+                </Col>
+                <Col span={12}>
+                    <Navbar />
+                </Col>
+                <Col span={6} style={{ textAlign: 'end' }}>
+                    <ProfileHeader />
+                </Col>
+            </Row>
+        </Col>
+    </Row>
+)
